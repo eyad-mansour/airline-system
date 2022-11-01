e0647d4c465e75e61a6e035ef7b8f0f0487be307
eyad-mansour <eyad.mansour.1997@gmail.com>
2022-11-01 18:34:00 +0300

    refactor the system ,manager,pilot

--- pilot.js
+++ pilot.js
@@ -1,14 +1,19 @@
 'use strict';
+
 const events = require('./events');
+require('./manager');
+require('./system');
 
-events.on('took-off', tookOff);
+events.on('newFlight', newFlight);
 
-function tookOff() {
-  let id = uuidv4();
-  let dateTime = `Pilot: flight with ID: ${id} ${Date()} took-off`;
-  console.log(dateTime);
+function newFlight(info) {
+  setTimeout(() => {
+    console.log(`Pilot: flight with ID '${info.flightNO}' took-off`);
+    events.emit('tookOff', info);
+  }, 4000);
+
+  setTimeout(() => {
+    console.log(`Pilot: flight with ID '${info.flightNO}' arrived`);
+    events.emit('arrived', info);
+  }, 7000);
 }
-
-setInterval(() => {
-  events.emit('took-off');
-}, 4000);
